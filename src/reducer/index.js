2665ed70c85da7e05880efa1dd066e23fd83e3fd
Ayesha W <ayesha.wnz@gmail.com>
2018-06-13 11:50:53 +1200

    Adds a reducer (init)

---
+++ src/reducer/index.js
@@ -0,0 +1,14 @@
+import { VisibilityFilters } from './actions'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todoApp(state, action) {
+  if (typeof state === 'undefined') {
+    return initialState
+  }
+  //For now, don't handle any actions and just return the state given to us
+  return state
+}
