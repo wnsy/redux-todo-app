4c4cb5022268dd1ca7c1f1a3c93548c007c6b824
Ayesha W <ayesha.wnz@gmail.com>
2018-06-13 13:29:38 +1200

    Adds case: add_todo

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,14 +1,38 @@
-import { VisibilityFilters } from './actions'
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters
+} from './actions'
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
   todos: []
 }
 
-function todoApp(state, action) {
-  if (typeof state === 'undefined') {
-    return initialState
+function todoApp(state = initialState, action) {
+  switch (action.type) { //.type = ?
+    case SET_VISIBILITY_FILTER:
+    return Object.assign({}, state, { // Object.assign() creates a copy because we
+      //don't mutate the state. It's part of ES6. MUST supply an empty object as the first param
+      //can also enable the object spread operator proposal to write {...state, ...newState} instead
+      visibilityFilter: action.filter
+    })
+    case ADD_TODO:
+    return Object.assign({}, state, {
+      todos: [
+        ...state.todos,
+        {
+          text: action.text,
+          completed: false //never write directly to state or its fields and instead we return new objects
+          //the new `todos` is equal to the old `todos` concatenatedd w/ a single
+          //new item at the end. The fresh todo was constructed using the data fr. the action.
+        }
+      ]
+    })
+
+    
+    default:
+    return state
   }
-  //For now, don't handle any actions and just return the state given to us
-  return state
 }
