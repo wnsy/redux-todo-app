eb80cc52c98071042510fa3c58f6b730f156e83c
Ayesha W <ayesha.wnz@gmail.com>
2018-06-14 11:15:21 +1200

    Implements reducer composition

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -10,6 +10,41 @@ const initialState = {
   todos: []
 }
 
+const { SHOW_ALL } = VisibilityFilters
+
+function VisibilityFilters(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+    return action.filter
+    default:
+    return state
+  }
+}
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+    return [
+      ...state,
+      {text: action.text,
+        completed: false
+      }
+    ]
+
+    case TOGGLE_TODO:
+    return state.map((todo, index) => {
+      if (index == action.index) {
+        return Object.assign({}, todo, {
+          completed: !todo.completed
+        })
+      }
+      return todo
+    }
+    default:
+    return state
+  }
+}
+
 function todoApp(state = initialState, action) {
   switch (action.type) { //.type = ?
     case SET_VISIBILITY_FILTER:
@@ -21,36 +56,26 @@ function todoApp(state = initialState, action) {
 
     //new `todos` is equal to the old `todos` concatenated
     case ADD_TODO:
-    return Object.assign({}, state, {
-      todos: [
-        ...state.todos,
-        {
-          text: action.text,
-          completed: false //never write directly to state or its fields and instead we return new objects
-          //the new `todos` is equal to the old `todos` concatenatedd w/ a single
-          //new item at the end. The fresh todo was constructed using the data fr. the action.
-        }
-      ]
-    })
+    return [
+      ...state,
+      {
+        text: action.text,
+        completed: false //never write directly to state or its fields and instead we return new objects
+        //the new `todos` is equal to the old `todos` concatenatedd w/ a single
+        //new item at the end. The fresh todo was constructed using the data fr. the action.
+
+      }
+    ]
 
     case TOGGLE_TODO:
-    // What does the part after `state` mean below?
-    return Object,assign({}, state, {
-      todos: state.todos.map()
-    })
-
-    case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo
+    return state.map((todo, index) => {
+      if (index === action.index) {
+        return Object.assign({}, todo, {
+          completed: !todo.completed
         })
-      })
-
+      }
+      return todo
+    })
 
     default:
     return state
