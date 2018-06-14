775891a6cf008ea4f36c0fd309878a29198b0ffb
Ayesha W <ayesha.wnz@gmail.com>
2018-06-14 14:50:00 +1200

    Changes todoApp function to use combineReducers()

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -14,7 +14,9 @@ const { SHOW_ALL } = VisibilityFilters
 
 function VisibilityFilters(state = SHOW_ALL, action) {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
+    case SET_VISIBILITY_FILTER: // Object.assign() creates a copy because we
+      //don't mutate the state. It's part of ES6. MUST supply an empty object as the first param
+      //can also enable the object spread operator proposal to write {...state, ...newState} instead
     return action.filter
     default:
     return state
@@ -23,47 +25,14 @@ function VisibilityFilters(state = SHOW_ALL, action) {
 
 function todos(state = [], action) {
   switch (action.type) {
-    case ADD_TODO:
-    return [
-      ...state,
-      {text: action.text,
-        completed: false
-      }
-    ]
-
-    case TOGGLE_TODO:
-    return state.map((todo, index) => {
-      if (index == action.index) {
-        return Object.assign({}, todo, {
-          completed: !todo.completed
-        })
-      }
-      return todo
-    }
-    default:
-    return state
-  }
-}
-
-function todoApp(state = initialState, action) {
-  switch (action.type) { //.type = ?
-    case SET_VISIBILITY_FILTER:
-    return Object.assign({}, state, { // Object.assign() creates a copy because we
-      //don't mutate the state. It's part of ES6. MUST supply an empty object as the first param
-      //can also enable the object spread operator proposal to write {...state, ...newState} instead
-      visibilityFilter: action.filter
-    })
-
     //new `todos` is equal to the old `todos` concatenated
     case ADD_TODO:
     return [
       ...state,
-      {
-        text: action.text,
+      {text: action.text,
         completed: false //never write directly to state or its fields and instead we return new objects
         //the new `todos` is equal to the old `todos` concatenatedd w/ a single
         //new item at the end. The fresh todo was constructed using the data fr. the action.
-
       }
     ]
 
@@ -75,9 +44,26 @@ function todoApp(state = initialState, action) {
         })
       }
       return todo
-    })
-
+    }
     default:
     return state
   }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+})
+
+export default todoApp // <--why default
+
+
+// // Note: each of these reducers is managing its own part of the global state.
+// // The `state` param is different for every reducer, and corresponds to the
+// // part of the state it manages
+// function todoApp(state = {}, action) {
+//   return {
+//     visibilityFilter: VisibilityFilters(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   }
 }
